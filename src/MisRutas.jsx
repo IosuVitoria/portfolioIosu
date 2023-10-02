bc5636c6f47ad6d8c32647289ea794a9313bff54
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2023-10-02 17:49:39 +0200

    fix: fix cannot access to specific route

--- src/MisRutas.jsx
+++ src/MisRutas.jsx
@@ -1,31 +1,32 @@
-import {Routes,  Route, BrowserRouter} from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import HeaderNav from '../src/components/layout/Header/HeaderNav';
 import Footer from './components/layout/Footer/Footer';
 import Curriculum from './components/Curriculum/Curriculum';
-import Contacto from "./components/Contacto/Contacto";
-import Inicio from "./components/Inicio/Inicio";
-import Portfolio from "./components/portfolio/Portfolio";
-import Blog from "./components/Blog/Blog";
-
+import Contacto from './components/Contacto/Contacto';
+import Inicio from './components/Inicio/Inicio';
+import Portfolio from './components/portfolio/Portfolio';
+import Blog from './components/Blog/Blog';
 
 const MisRutas = () => {
   return (
-   <BrowserRouter basename="/portfolioIosu">
-        {/* {Header y navegación} */}
-        <HeaderNav />
-        {/* Contenido central */}
-        <Routes>
-            <Route path="/" element={<Inicio />}/>
-            <Route path="/inicio" element={<Inicio />}/>
-            <Route path="/portfolio" element={<Portfolio />}/>
-            <Route path="/blog" element={<Blog />}/>
-            <Route path="/curriculum" element={<Curriculum />}/>
-            <Route path="/contacto" element={<Contacto />}/>
-        </Routes>
-         {/* Pie de página */}
-        <Footer />
-   </BrowserRouter>
-  )
-}
+    <BrowserRouter basename='/portfolioIosu'>
+      {/* {Header y navegación} */}
+      <HeaderNav />
+      {/* Contenido central */}
+      <Routes>
+        <Route path='/' element={<Inicio />}>
+          <Route path='/' element={<Navigate to='/inicio' />} />
+          <Route path='/inicio' element={<Inicio />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/curriculum' element={<Curriculum />} />
+          <Route path='/contacto' element={<Contacto />} />ç
+        </Route>
+      </Routes>
+      {/* Pie de página */}
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default MisRutas
+export default MisRutas;
